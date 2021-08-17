83496d0fd1b927c933e360180c441aeeabe64998
Ravi Pratap Singh <68327599+cyberspatial@users.noreply.github.com>
2021-08-17 13:30:50 +0530

    Add files via upload

---
+++ E-SERVICES/FLAGSHIP PROJECTS/Script/script.js
@@ -0,0 +1,47 @@
+function updatetime()
+{
+    const d = new Date();
+document.getElementsByClassName("time")[0].innerHTML = d;
+}
+setInterval(updatetime,1000);
+ 
+function openNav()
+{
+    var m = document.getElementsByClassName("menu-bar-small-device");
+    var l =m[0];
+    if(l.style.display==="flex"){
+        l.style.display="none";
+    }
+    else 
+    {
+        l.style.display="flex";
+    }
+}
+
+
+   
+function shownestedlinkcontact()
+{
+    var nested= document.getElementsByClassName("unordered-nested-contact");
+    var nested_first =nested[0];
+    if(nested_first.style.display==="flex"){
+        nested_first.style.display="none";
+    }
+    else 
+    {
+    nested_first.style.display="flex";
+    }
+}
+
+function shownestedlinkimp()
+{
+    var nested= document.getElementsByClassName("unordered-nested-imp");
+    var nested_first =nested[0];
+    if(nested_first.style.display==="flex"){
+        nested_first.style.display="none";
+    }
+    else 
+    {
+    nested_first.style.display="flex";
+    }
+}
